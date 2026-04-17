646ca6351f1ddefaa89e071099d273fab47be418
mrmateussiilva <mateuspython3010@gmail.com>
2026-04-16 21:44:45 -0300

    fix: adicionar tag querystring ao string_filters e corrigir import no modal

--- core/templatetags/string_filters.py
+++ core/templatetags/string_filters.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django import template
 
 register = template.Library()
@@ -7,3 +9,12 @@ register = template.Library()
 def make_list(value):
     """Converte uma string em lista de caracteres."""
     return list(str(value))
+
+
+@register.simple_tag
+def querystring(params, **kwargs):
+    """Adiciona ou substitui parâmetros na query string."""
+    query = dict(params)
+    query.update(kwargs)
+    query = {k: v for k, v in query.items() if v}
+    return urlencode(query)
